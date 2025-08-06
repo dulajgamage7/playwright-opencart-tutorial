5a8d6b67f330c71bcc133fca28721387ce91949c
dulaj <gamage.dulaj@workz.com>
2025-08-06 09:12:35 +0400

    Added pageObjectManager class to centralize page object creation and access

--- tests/support/utils/pageObjectManager.js
+++ tests/support/utils/pageObjectManager.js
@@ -2,24 +2,29 @@ const { page } = require("@playwright/test");
 const { LoginPage } = require("../page-objects/loginPage.js");
 const { AddToCartPage } = require("../page-objects/addToCartPage.js");
 
+// This class manages and provides access to all page objects.
+// It helps centralize page object creation and reuse them across tests.
 class pageObjectManager {
-  // Declare private fields
- 
+  // Declare private fields for encapsulating page object instances
   #loginPage;
   #addToCartPage;
 
+  // Constructor initializes all required page objects with the shared 'page' instance
   constructor(page) {
     this.page = page;
     this.#loginPage = new LoginPage(page);
     this.#addToCartPage = new AddToCartPage(page);
   }
 
+  // Returns the LoginPage object
   loginPage() {
     return this.#loginPage;
   }
 
+  // Returns the AddToCartPage object
   addToCartPage() {
     return this.#addToCartPage;
   }
 }
+
 module.exports = { pageObjectManager };
